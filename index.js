5ae356f21d417486864ff2430a5625f846027f9c
aaafasf <csc.calle@yavirac.edu.ec>
2025-10-16 21:10:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-function sumar(a,b) {
-    return a+b;
+function sumar(a, b) {
+    return a + b;
 }
 
-console.log("Resultado final:",sumar(2,3));
-module.exports = sumar; 
+module.exports = sumar;
